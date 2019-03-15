a5198ef9bca586ef903e71cd2a97b66f9c1d0a59
Akhil Nair <akhil.nair@revolut.co.uk>
2019-03-15 17:51:51 +0000

    Set metadata on collectd to create metrics on stackdriver

--- collectd_rabbitmq_monitoring/__init__.py
+++ collectd_rabbitmq_monitoring/__init__.py
@@ -29,22 +29,22 @@ def configure(configobj):
     global queues_to_count
 
     config = {c.key: c.values for c in configobj.children}
-    INTERVAL = config['interval'][0]
-    host = config['host'][0]
-    port = int(config['port'][0])
-    username = config['username'][0]
-    password = config['password'][0]
+    INTERVAL = config["interval"][0]
+    host = config["host"][0]
+    port = int(config["port"][0])
+    username = config["username"][0]
+    password = config["password"][0]
     queues_to_count = []
-    if 'message_count' in config:
-        queues_to_count = config['message_count']
-    collectd.info('rabbitmq_monitoring: Interval: {}'.format(INTERVAL))
-    cl = Client('{}:{}'.format(host, port), username, password)
+    if "message_count" in config:
+        queues_to_count = config["message_count"]
+    collectd.info("rabbitmq_monitoring: Interval: {}".format(INTERVAL))
+    cl = Client("{}:{}".format(host, port), username, password)
     collectd.info(
-        'rabbitmq_monitoring: Connecting to: {}:{} as user:{} password:{}'
-        .format(host, port, username, password))
-    collectd.info(
-        'rabbitmq_monitoring: Counting messages on: {}'
-        .format(queues_to_count))
+        "rabbitmq_monitoring: Connecting to: {}:{} as user:{} password:{}".format(
+            host, port, username, password
+        )
+    )
+    collectd.info("rabbitmq_monitoring: Counting messages on: {}".format(queues_to_count))
     collectd.register_read(read, INTERVAL)
 
 
@@ -54,97 +54,123 @@ def read(data=None):
     overview = cl.get_overview()
 
     # Object counts
-    for m_instance in \
-            ['channels', 'connections', 'consumers', 'exchanges', 'queues']:
-        if m_instance in overview['object_totals']:
+    type_ = "object_totals"
+    for m_instance in ["channels", "connections", "consumers", "exchanges", "queues"]:
+        if m_instance in overview[type_]:
             metric = collectd.Values()
-            metric.plugin = 'rabbitmq_monitoring'
+            metric.meta = {
+                "stackdriver_metric_type": "custom.googleapis.com/rabbitmq_monitoring/{}/{}".format(
+                    m_instance, type_
+                )
+            }
+            metric.plugin = "rabbitmq_monitoring"
             metric.interval = INTERVAL
-            metric.type = 'gauge'
+            metric.type = "gauge"
             metric.type_instance = m_instance
-            metric.values = [overview['object_totals'][m_instance]]
+            metric.values = [overview["object_totals"][m_instance]]
             metric.dispatch()
 
     # Aggregated Queue message stats
-    for m_instance in \
-            ['messages', 'messages_ready', 'messages_unacknowledged']:
-        if m_instance in overview['queue_totals']:
+    type_ = "queue_totals"
+    for m_instance in ["messages", "messages_ready", "messages_unacknowledged"]:
+        if m_instance in overview[type_]:
             metric = collectd.Values()
-            metric.plugin = 'rabbitmq_monitoring'
+            metric.meta = {
+                "stackdriver_metric_type": "custom.googleapis.com/rabbitmq_monitoring/{}/{}-{}".format(
+                    m_instance, type_, "count"
+                )
+            }
+            metric.plugin = "rabbitmq_monitoring"
             metric.interval = INTERVAL
-            metric.type = 'gauge'
-            metric.type_instance = 'queue_total-{}-count'.format(m_instance)
-            metric.values = [overview['queue_totals'][m_instance]]
+            metric.type = "gauge"
+            metric.type_instance = "queue_total-{}-count".format(m_instance)
+            metric.values = [overview[type_][m_instance]]
             metric.dispatch()
 
             metric = collectd.Values()
-            metric.plugin = 'rabbitmq_monitoring'
+            metric.meta = {
+                "stackdriver_metric_type": "custom.googleapis.com/rabbitmq_monitoring/{}/{}-{}".format(
+                    m_instance, type_, "rate"
+                )
+            }
+            metric.plugin = "rabbitmq_monitoring"
             metric.interval = INTERVAL
-            metric.type = 'gauge'
-            metric.type_instance = 'queue_total-{}-rate'.format(
-                m_instance)
-            metric.values = \
-                [
-                    overview['queue_totals']['{}_details'.format(m_instance)]
-                    ['rate']
-                ]
+            metric.type = "gauge"
+            metric.type_instance = "queue_total-{}-rate".format(m_instance)
+            metric.values = [overview["queue_totals"]["{}_details".format(m_instance)]["rate"]]
             metric.dispatch()
 
     # Aggregated Message Stats
-    for m_instance in \
-            [
-                'ack', 'confirm', 'deliver', 'deliver_get', 'deliver_no_ack',
-                'get', 'get_no_ack', 'publish', 'publish_in', 'publish_out',
-                'redeliver', 'return_unroutable'
-            ]:
-        if m_instance in overview['message_stats']:
+    type_ = "message_stats"
+    for m_instance in [
+        "ack",
+        "confirm",
+        "deliver",
+        "deliver_get",
+        "deliver_no_ack",
+        "get",
+        "get_no_ack",
+        "publish",
+        "publish_in",
+        "publish_out",
+        "redeliver",
+        "return_unroutable",
+    ]:
+        if m_instance in overview[type_]:
             metric = collectd.Values()
-            metric.plugin = 'rabbitmq_monitoring'
+            metric.meta = {
+                "stackdriver_metric_type": "custom.googleapis.com/rabbitmq_monitoring/{}/{}-{}".format(
+                    m_instance, type_, "count"
+                )
+            }
+            metric.plugin = "rabbitmq_monitoring"
             metric.interval = INTERVAL
-            metric.type = 'gauge'
-            metric.type_instance = 'message_total-{}-count'.format(m_instance)
-            metric.values = [overview['message_stats'][m_instance]]
+            metric.type = "gauge"
+            metric.type_instance = "message_total-{}-count".format(m_instance)
+            metric.values = [overview["message_stats"][m_instance]]
             metric.dispatch()
 
             metric = collectd.Values()
-            metric.plugin = 'rabbitmq_monitoring'
+            metric.meta = {
+                "stackdriver_metric_type": "custom.googleapis.com/rabbitmq_monitoring/{}/{}-{}".format(
+                    m_instance, type_, "rate"
+                )
+            }
+            metric.plugin = "rabbitmq_monitoring"
             metric.interval = INTERVAL
-            metric.type = 'gauge'
-            metric.type_instance = 'message_total-{}-rate'.format(m_instance)
-            metric.values = \
-                [
-                    overview['message_stats']['{}_details'.format(m_instance)]
-                    ['rate']
-                ]
+            metric.type = "gauge"
+            metric.type_instance = "message_total-{}-rate".format(m_instance)
+            metric.values = [overview["message_stats"]["{}_details".format(m_instance)]["rate"]]
             metric.dispatch()
 
     # Configurable per-queue message counts
     for queue_name in queues_to_count:
         messages_detail = None
         try:
-            messages_detail = cl.get_messages('/', queue_name)
+            messages_detail = cl.get_messages("/", queue_name)
         except HTTPError as err:
-            collectd.error(
-                'Error Opening Queue [{}] details: {}'
-                .format(queue_name, err))
+            collectd.error("Error Opening Queue [{}] details: {}".format(queue_name, err))
         if messages_detail is None:
             count = 0
         else:
-            count = messages_detail[0]['message_count']
+            count = messages_detail[0]["message_count"]
         metric = collectd.Values()
-        metric.plugin = 'rabbitmq_monitoring'
+        metric.meta = {
+            "stackdriver_metric_type": "custom.googleapis.com/rabbitmq_monitoring/{}/{}".format(
+                "msg_count", queue_name
+            )
+        }
+        metric.plugin = "rabbitmq_monitoring"
         metric.interval = INTERVAL
-        metric.type = 'gauge'
-        metric.type_instance = 'msg_count-{}'.format(queue_name)
+        metric.type = "gauge"
+        metric.type_instance = "msg_count-{}".format(queue_name)
         metric.values = [count]
         metric.dispatch()
 
     timediff = time.time() - starttime
     if timediff > INTERVAL:
-        collectd.warning(
-            'rabbitmq_monitoring: Took: {} > {}'.format(
-                round(timediff, 2),
-                INTERVAL)
-            )
+        collectd.warning("rabbitmq_monitoring: Took: {} > {}".format(round(timediff, 2), INTERVAL))
+
 
 collectd.register_config(configure)
+
